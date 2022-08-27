89f8a3507d695e583190fcac12850cf9c5e2857b
Rakibul Kabir <107674050+fsRakib@users.noreply.github.com>
2022-08-27 10:24:30 +0600

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    char ch1[100],ch2[100];
+    int i, j, count=0;
+    fgets(ch1, 100, stdin);
+    puts(ch1);
+    for (i=0; (ch1[i]!=0); i++)
+    {
+        count++;
+    }
+  //  j=count-1;
+    for(i=0,j=count-1; i<count; i++,j--)
+    {
+        ch2[j]=ch1[i];
+    }
+    puts(ch2);
+    return 0;
+}
