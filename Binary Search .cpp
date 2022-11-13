a097e9dfb7ef7973db4db31e7664943ef04770a6
Rakibul Kabir <107674050+fsRakib@users.noreply.github.com>
2022-11-13 20:02:20 +0600

    Add files via upload

---
+++ Binary Search .cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int binarySearch(int arr[], int n, int key)
+{
+    int start=0;
+    int End=n;
+    while(start<=End)
+    {
+        int mid=(start+End)/2;
+        if(arr[mid]==key)
+            return mid;
+        else if(arr[mid]>key)
+            End=mid-1;
+        else
+            start=mid+1;
+    }
+    return -1;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0; i<n; i++)
+        cin>>arr[i];
+    int key;
+    cin>>key;
+    cout<<"Index :: "<<binarySearch(arr, n, key)<<endl;
+    return 0;
+}
