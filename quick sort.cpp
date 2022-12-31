0eb4dff240ddcf278d8daa92cdaa8d713cf2912b
Rakibul Kabir <107674050+fsRakib@users.noreply.github.com>
2022-12-31 22:26:37 +0600

    Update quick sort.cpp

--- quick sort.cpp
+++ quick sort.cpp
@@ -5,26 +5,29 @@ the pivot element at its correct position in sorted
 array, and places all smaller (smaller than pivot)
 to left of pivot and all greater elements to right
 of pivot */
-int partition(int arr[], int low, int high)
+int partition (int arr[], int low, int high)
 {
     int pivot=arr[high];
     int i=low-1;
-    for(int j=0; j<high; j++)
-    {                          ////////////////////
-        if(arr[j]<pivot)      /////Placing/////////
-            i++;             /////Smaller/////////
-        //swap               //////element////////
-       int temp=arr[i];     //////Before/////////
-        arr[i]=arr[j];      ///////pivot/////////
-        arr[j]=temp;        ////////////////////
-       
+    for(int j=low; j<high; j++)
+    {
+        if(arr[j]<pivot)
+        {
+                                /////Placing/////////
+            i++;               /////Smaller/////////
+            //swap            //////element////////
+            int temp=arr[i]; //////Before/////////
+            arr[i]=arr[j];  ///////pivot/////////
+            arr[j]=temp;   ////////////////////
+        }
     }
-    i++;                 /////Placing////////
-   int temp=arr[i];     //////PIVOT///////
+    i++;                  /////Placing////////
+    int temp=arr[i];     //////PIVOT///////
     arr[i]=pivot;       ////////in//////////
-    arr[high]=temp;     ///Right Place//////
-   
+    arr[high]=temp;    ///Right Place//////
+
     return i; //------------->> Pivot Index////
+
 }
 void quicksort(int arr[], int low, int high)
 {
@@ -33,6 +36,7 @@ void quicksort(int arr[], int low, int high)
     if(low<high)
     {
         int pivdx=partition(arr, low, high);
+
         quicksort(arr, low, pivdx-1);
         quicksort(arr, pivdx+1, high);
     }
@@ -45,7 +49,7 @@ int main()
     for(int i=0; i<n; i++)
         cin>>arr[i];
     quicksort(arr, 0, n - 1);
-     for(int i=0; i<n; i++)
+    for(int i=0; i<n; i++)
         cout<<arr[i]<<" ";
     return 0;
 }
